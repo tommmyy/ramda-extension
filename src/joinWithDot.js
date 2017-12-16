8bfc0e2987cd3bde794505fe720cec556f88af44
Jiří Brádle <bradle.jiri@gmail.com>
2017-12-16 21:25:50 +0100

    Join with dot
    Signed-off-by: Jiří Brádle <bradle.jiri@gmail.com>

---
+++ src/joinWithDot.js
@@ -0,0 +1,17 @@
+import { join } from 'ramda';
+
+/**
+ * Joins array of string with dot determiner.
+ *
+ * @func
+ * @category String
+ *
+ * @example
+ *
+ * 		R_.joinWithDot(['a', 'b', 'c']) // 'a.b.c'
+ *
+ * @sig [String] -> String
+ */
+const joinWithDot = join('.');
+
+export default joinWithDot;

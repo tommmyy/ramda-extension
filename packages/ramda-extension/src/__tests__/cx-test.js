d5e8d258b65be014b7e78465f7c59cb3bdccf36d
aizerin <l.nuzky@gmail.com>
2018-04-12 14:19:18 +0200

    added perf benchmark for cx

--- packages/ramda-extension/src/__tests__/cx-test.js
+++ packages/ramda-extension/src/__tests__/cx-test.js
@@ -1,4 +1,9 @@
+/**
+ * @jest-environment node
+ */
 import { cx } from '../';
+import Benchmark from 'benchmark';
+import classNames from 'classnames';
 
 describe('cx', () => {
 	it('should exists', () => {
@@ -24,5 +29,31 @@ describe('cx', () => {
 	});
 	it('should handle a mix of strings, arrays, and objects', () => {
 		expect(cx(['one', ['two'], { three: true, four: 0 }], 'five six', { two: false })).toBe('one five six three');
+	});
+	xit('should be fast as classnames +- 100 ops/sec', () => {
+		return new Promise((resolve, reject) => {
+			const suite = new Benchmark.Suite();
+			const args = ['foo', { bar: true, duck: false }, 'baz', { quux: true }, ['b', { c: true, d: false }]];
+			suite
+				.add('classNames', function() {
+					classNames(...args);
+				})
+				.add('cx', function() {
+					cx(...args);
+				})
+				// .on('cycle', function(event) {
+				// console.log(String(event.target));
+				// })
+				.on('complete', function() {
+					// console.log(`Fastest is ${this.filter('fastest').map('name')}`);
+					const classNamesOps = this['0'].hz;
+					const cxOps = this['1'].hz;
+					if (cxOps >= classNames - 100) {
+						resolve();
+					}
+					reject(`cx is about ${Math.round(classNamesOps / cxOps)} times slower`);
+				})
+				.run();
+		});
 	});
 });

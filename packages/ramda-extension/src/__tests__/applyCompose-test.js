b6289bbb897291a0aa2451446d36ddce80f01766
Tomáš Konrády <tomaskonrady@gmail.com>
2018-05-15 08:54:19 +0200

    typo

--- packages/ramda-extension/src/__tests__/applyCompose-test.js
+++ packages/ramda-extension/src/__tests__/applyCompose-test.js
@@ -3,7 +3,7 @@ import { multiply, add } from 'ramda';
 import { applyCompose } from '../';
 
 describe('applyCompose', () => {
-	it('should apply pipe to list of functions', () => {
+	it('should apply compose to list of functions', () => {
 		expect(applyCompose([multiply(2), add(1)])(3)).toBe(8);
 	});
 });

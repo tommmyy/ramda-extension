d7ef1879286497c77eb4c1a748c2f757994c6461
Nicolás Fantone <nfantone@gmail.com>
2018-06-23 14:32:14 +0100

    add new alwaysNew internal based on thunkify

---
+++ packages/ramda-extension/src/internal/thunkify.js
@@ -0,0 +1,19 @@
+import { curryN } from 'ramda';
+
+/**
+ * Creates a thunk out of a function. A thunk delays a calculation until
+ * its result is needed, providing lazy evaluation of arguments.
+ *
+ * @func
+ * @example
+ *
+ *      R_.thunkify(R.identity)(42)(); //=> 42
+ *      R_.thunkify((a, b) => a + b)(25, 17)(); //=> 42
+ * @sig ((a, b, ..., j) -> k) -> (a, b, ..., j) -> (() -> k)
+ */
+const thunkify = (fn) => curryN(fn.length, (...args) => () => fn(...args));
+
+// NOTE (23/06/18): `thunkify` was added to Ramda core but it's
+// still unreleased. We should remove this module once it becomes available.
+// @see https://github.com/ramda/ramda/pull/2551
+export default thunkify;

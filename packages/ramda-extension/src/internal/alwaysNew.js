2cbde1c28b3c10333571e651ca945c97ce29cb5a
Nicolás Fantone <nfantone@gmail.com>
2018-06-23 14:32:14 +0100

    add new alwaysNew internal based on thunkify

---
+++ packages/ramda-extension/src/internal/alwaysNew.js
@@ -0,0 +1,19 @@
+import { clone } from 'ramda';
+import thunkify from '../internal/thunkify';
+
+/**
+ * Returns a function that creates new instances of whatever argument
+ * is passed in each time it's called.
+ *
+ * @func
+ * @example
+ *
+ * 	const alwaysNewArray = alwaysNew([]);
+ *  const a = alwaysNewArray();
+ *  const b = alwaysNewArray();
+ *  // a !== b
+ *
+ */
+const alwaysNew = thunkify(clone);
+
+export default alwaysNew;

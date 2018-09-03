c00a969ff8a762fa3f0efd0940fc92b601eef605
aizerin <l.nuzky@gmail.com>
2018-09-03 12:11:37 +0200

    added default args function

---
+++ packages/ramda-extension/src/__tests__/defaultArgs-test.js
@@ -0,0 +1,37 @@
+import { concat, always } from 'ramda';
+import defaultArgs from '../defaulArgs';
+
+describe('defaultArgs', () => {
+	const calledFn = (...args) => args;
+	it('should default arguments - args and input same length', () => {
+		expect(
+			defaultArgs(['default1', 'default2'], calledFn)(
+				undefined,
+				'defined'
+			)
+		).toEqual(['default1', 'defined']);
+	});
+	it('should default arguments - args are bigger than input', () => {
+		expect(
+			defaultArgs(['default1', 'default2', 'default3'], calledFn)(
+				undefined,
+				'defined'
+			)
+		).toEqual(['default1', 'defined', 'default3']);
+	});
+	it('should default arguments - input is bigger than args', () => {
+		expect(
+			defaultArgs(['default1', 'default2'], calledFn)(
+				undefined,
+				'defined',
+				'also defined'
+			)
+		).toEqual(['default1', 'defined', 'also defined']);
+	});
+	it('should be ac when used on concat', () => {
+		expect(defaultArgs(['a', 'b'], concat)(undefined, 'c')).toEqual('ac');
+	});
+	it('should be {} when used on always', () => {
+		expect(defaultArgs([{}], always)(undefined)()).toEqual({});
+	});
+});

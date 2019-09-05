14dd76e88117cd64ac9070b88bdf16ce7cd6f065
Lukas Duspiva <lukas.duspiva@gmail.com>
2019-09-05 22:25:29 +0200

    New function - decapitalizeAll

--- packages/ramda-extension/src/capitalizeFirst.js
+++ packages/ramda-extension/src/capitalizeFirst.js
@@ -7,7 +7,7 @@ import { replace, toUpper, unless, isNil } from 'ramda';
  * @category String
  *
  * @param {any} x Any string
- * @return {any} Description
+ * @return {any} String startinng with upper-case letter
  *
  * @example
  *

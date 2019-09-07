69abe41c92d490548966f85aada5da6dfdadb7f3
Lukas Duspiva <lukas.duspiva@gmail.com>
2019-09-07 12:28:05 +0200

    New function - decapitalizeAll - docs fix

--- packages/ramda-extension/src/decapitalizeAll.js
+++ packages/ramda-extension/src/decapitalizeAll.js
@@ -7,7 +7,7 @@ import { replace, toLower, unless, isNil } from 'ramda';
  * @category String
  *
  * @param {any} x Any string
- * @return {any} Description
+ * @return {any} String where every word starts with lower-case letter
  *
  * @example
  *

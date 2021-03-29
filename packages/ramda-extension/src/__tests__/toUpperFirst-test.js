62aa1bf2a6c140751ad12ffa2c83e4ec4f515921
Tomáš Konrády <tomaskonrady@gmail.com>
2021-03-29 13:01:00 +0200

    fix(ramda-extension): toUpperFirst failing with empty string

--- packages/ramda-extension/src/__tests__/toUpperFirst-test.js
+++ packages/ramda-extension/src/__tests__/toUpperFirst-test.js
@@ -4,4 +4,7 @@ describe('toUpperFirst', () => {
 	it('capitalize first letter', () => {
 		expect(toUpperFirst('hello')).toBe('Hello');
 	});
+	it('works with empty strings', () => {
+		expect(toUpperFirst('')).toBe('');
+	});
 });
